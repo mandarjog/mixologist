772e9520b9dd5e2c789595555aa63dc8a431eb42
Mandar U Jog <mjog@google.com>
2016-08-24 08:16:10 -0700

    add ReportRequest channel consumer

--- mixologist/prometheus.go
+++ mixologist/prometheus.go
@@ -1,15 +1,30 @@
 package mixologist
 
 import (
+	"github.com/golang/glog"
+	"github.com/prometheus/client_golang/prometheus"
 	sc "google/api/servicecontrol/v1"
 )
 
 type PrometheusReporter struct {
 	ReportQueue chan sc.ReportRequest
+	MetricMap   map[string]*prometheus.SummaryVec
 }
 
 func NewPrometheusReporter() ReportConsumer {
-	return &PrometheusReporter{}
+	latency := prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: "api_producer_total_latencies",
+			Help: "serviceruntime.googleapis.com/api/producer/total_latencies",
+		},
+		[]string{"service"},
+	)
+	mm := make(map[string]*prometheus.SummaryVec)
+	mm["serviceruntime.googleapis.com/api/producer/total_latencies"] = latency
+	prometheus.MustRegister(latency)
+	return &PrometheusReporter{
+		MetricMap: mm,
+	}
 }
 
 func (p *PrometheusReporter) SetReportQueue(ch chan sc.ReportRequest) {
@@ -19,7 +34,19 @@ func (p *PrometheusReporter) SetReportQueue(ch chan sc.ReportRequest) {
 // Do prometheus specific processing here
 // Start a new listener etc.
 func (p *PrometheusReporter) Start() {
-
+	glog.Info("Staring prometheus loop")
+	for reportMsg := range p.ReportQueue {
+		for _, oprn := range reportMsg.GetOperations() {
+			for _, metric := range oprn.GetMetricValueSets() {
+				if summvec, ok := p.MetricMap[metric.MetricName]; ok {
+					val := metric.MetricValues[0].GetDoubleValue()
+					glog.Info(val)
+					glog.Info(metric.MetricValues[0])
+					summvec.WithLabelValues("producer").Observe(val)
+				}
+			}
+		}
+	}
 }
 
 // Stop Processing
